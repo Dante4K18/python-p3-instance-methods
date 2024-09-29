fe2463ce0fe37fd1b5de8d7530bb1edb08c916ba
Dante4K18 <dan@dan>
2024-09-29 23:31:46 +0300

    completed-instance-methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def __init__(self, name):
+        self.name = name
+
+        def sit(self):
+            print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+
